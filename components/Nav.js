ee440fa220935243ba7a74ebf6c3cb7acc52e78d
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2022-02-15 19:25:09 +0000

    feat(nav): added desktop nav

--- components/Nav.js
+++ components/Nav.js
@@ -5,88 +5,91 @@ import { useEffect, useRef, useState } from 'react'
 import { Home, Page, GitHub, Book, Figma } from '@/components/Icons'
 
 const Nav = () => {
-  let desktop = useRef(false)
+  const [desktop, setDesktop] = useState(false)
+  const [navOpen, setNavOpen] = useState(false)
+
   useEffect(() => {
     if (window.innerWidth >= 768) {
-      desktop.current = true
+      setDesktop(true)
     }
   }, [])
 
-  const [navOpen, setNavOpen] = useState(false)
-
   return (
-    <header className='fixed top-0 -left-0 w-full bg-white z-20'>
+    <header className='fixed top-0 -left-0 w-full bg-white bg-opacity-90 z-20 backdrop-filter backdrop-blur-md '>
       <nav className='flex justify-between md:max-w-4xl md:mx-auto'>
         <Link href='/'>
           <a className='flex font-outfit z-20 h-14 w-14 items-center justify-center'>
             RM
           </a>
         </Link>
-        {!desktop.current && (
+        {!desktop && (
           <NavButton
             active={navOpen}
             openFunction={() => setNavOpen(!navOpen)}
           />
         )}
-        {!desktop.current && navOpen && (
-          <div className='mobile-nav z-10'>
-            <ul>
-              <li>
-                <Link href='/'>
-                  <a>
-                    <Home />
-                    Home &amp; About
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href='/projects'>
-                  <a>
-                    <Page />
-                    Projects &amp; Work
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href='/ideas'>
-                  <a>
-                    {' '}
-                    <Page />
-                    Design Ideas
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href='/code'>
-                  <a>
-                    <GitHub />
-                    Code
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href='/books'>
-                  <a>
-                    <Book />
-                    Books
-                  </a>
-                </Link>
-              </li>
-              <li>
-                <Link href='/figma'>
-                  <a>
-                    <Figma />
-                    Figma
-                  </a>
-                </Link>
-              </li>
-            </ul>
+        <div
+          className={`${desktop ? 'desktop-nav' : 'mobile-nav'} ${
+            navOpen ? 'nav-open' : ''
+          }`}
+        >
+          <ul className='md:pr-4'>
+            <li>
+              <Link href='/'>
+                <a>
+                  <Home />
+                  {desktop ? 'Home' : 'Home & About'}
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/projects'>
+                <a>
+                  <Page />
+                  {desktop ? 'Projects' : 'Projects & Work'}
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/ideas'>
+                <a>
+                  <Page />
+                  {desktop ? 'Ideas' : 'Design Ideas'}
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/code'>
+                <a>
+                  <GitHub />
+                  Code
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/books'>
+                <a>
+                  <Book />
+                  Books
+                </a>
+              </Link>
+            </li>
+            <li>
+              <Link href='/figma'>
+                <a>
+                  <Figma />
+                  Figma
+                </a>
+              </Link>
+            </li>
+          </ul>
 
+          {!desktop && (
             <div className='social-nav absolute flex left-0 w-full bottom-8 justify-center'>
               <SocialNav />
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </nav>
     </header>
   )
