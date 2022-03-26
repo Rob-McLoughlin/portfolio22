558100cc0388c442484af93337d0f7f1b3540b09
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2022-03-26 22:40:02 +0000

    build(page): building testing page for components

---
+++ pages/component-testing.js
@@ -0,0 +1,17 @@
+import Card from '@/molecules/Card'
+import { Box } from '@/atoms/Icon'
+const CompontentTesting = ({}) => {
+  return (
+    <section>
+      <Card
+        title='Card'
+        subtitle='Card Subtitle'
+        body='Card body'
+        location='Side Project'
+        icon={<Box />}
+      />
+    </section>
+  )
+}
+
+export default CompontentTesting
