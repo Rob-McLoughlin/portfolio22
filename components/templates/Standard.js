9311b388ba3135080c3cbc738666adf48cf3a0b2
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2022-03-27 18:32:39 +0100

    feat(Standard): adding option to hide nav on some pages

--- components/templates/Standard.js
+++ components/templates/Standard.js
@@ -1,12 +1,24 @@
 import Nav from '@/organisms/Nav'
 import Footer from '@/organisms/Footer'
+import { useEffect, useState } from 'react'
 
-const Standard = ({ children }) => (
-  <div>
-    <Nav />
-    <main className='standard'>{children}</main>
-    <Footer />
-  </div>
-)
+const hideNavOnPages = ['/login']
+
+const Standard = ({ children }) => {
+  const [hideNav, setHideNav] = useState(false)
+  useEffect(() => {
+    const path = window.location.pathname
+    if (hideNavOnPages.includes(path)) {
+      setHideNav(true)
+    }
+  }, [])
+  return (
+    <div>
+      {!hideNav && <Nav />}
+      <main className='standard'>{children}</main>
+      <Footer />
+    </div>
+  )
+}
 
 export default Standard
