577351b9feb3070c33da415cb0c22c6bc7785c76
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2022-03-27 19:17:17 +0100

    feat(Overlay): added Overlay backdrop comp

--- pages/component-testing.js
+++ pages/component-testing.js
@@ -1,10 +1,12 @@
 import Card from '@/molecules/Card'
 import Input from '@/atoms/Input'
 import Button from '@/atoms/Button'
+import OverlayBackdrop from '@/atoms/OverlayBackdrop'
 import { Box, RightArrow } from '@/atoms/Icon'
 const CompontentTesting = ({}) => {
   return (
     <section>
+      <OverlayBackdrop />
       <Card
         title='Card'
         subtitle='Card Subtitle'
