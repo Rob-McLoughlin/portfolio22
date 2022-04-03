77a202e03a996c45bfd3fd830ff661d115718ea5
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2022-04-03 19:39:30 +0100

    feat(analytics): added GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,21 @@
 import '@/styles/globals.scss'
 import Standard from '@/templates/Standard'
+import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import TagManager from 'react-gtm-module'
+
+const tagManagerArgs = {
+  gtmId: 'GTM-545LPZB'
+}
 
 export default function MyApp ({ Component, pageProps }) {
   const router = useRouter()
   const hideNavOnPages = ['/login']
+
+  useEffect(() => {
+    // Tag Manager
+    TagManager.initialize(tagManagerArgs)
+  }, [router.events])
 
   return (
     <Standard hideNav={hideNavOnPages.includes(router.pathname)}>
