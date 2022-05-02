66d573199c163aaa7b5d2531e536804263c65528
Rob-McLoughlin <robbiemcloughlin@gmail.com>
2022-05-02 20:06:31 +0100

    fix(Nav): fixed order of fills for blurs

--- components/organisms/Nav.js
+++ components/organisms/Nav.js
@@ -55,10 +55,10 @@ const Nav = () => {
 
   return (
     <header
-      className={`fixed top-0 -left-0 w-full bg-opacity-90 z-20 backdrop-filter backdrop-blur-md overflow-hidden ${navOpen &&
+      className={`fixed top-0 -left-0 w-full bg-white bg-opacity-80 z-20 backdrop-filter backdrop-blur-md overflow-hidden ${navOpen &&
         'h-full nav-open'}`}
     >
-      <nav className='flex justify-between md:max-w-3xl md:mx-auto overflow-hidden bg-white'>
+      <nav className='flex justify-between md:max-w-3xl md:mx-auto overflow-hidden'>
         <Link href='/'>
           <a
             className={`flex font-outfit z-20 h-14 w-14 items-center justify-center hover:text-flamingo transition ${!showLogo &&
